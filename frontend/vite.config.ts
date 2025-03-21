f5c1c8c1a13d9980be737bfdd7e2c54bb8eaaba5
chvg1968 <tu_email@ejemplo.com>
2025-03-21 16:46:37 -0500

    Update files config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,13 +20,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  },
-  // Deshabilitar verificación de tipos durante la compilación
-  esbuild: {
-    tsconfigRaw: {
-      compilerOptions: {
-        skipLibCheck: true
-      }
-    }
   }
 })
